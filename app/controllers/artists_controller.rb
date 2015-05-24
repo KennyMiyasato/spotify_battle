30d13501d38c375fc4dc7e49398163f4037e8054
xichi <xichi@github.com>
2015-05-24 16:27:19 -0400

    added comments and explanation to the controller model

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,23 +8,26 @@ require 'rspotify'
   end
 
   def results
+    # Saves artist name AKA "Hardwell" to be called later in the results page
     @artist_1 = params["artist_1"]
     @artist_2 = params["artist_2"]
 
-    # require 'pry'
-    # binding.pry
-
+    # Saves all results for artist AKA "Hardwell" and all associated artists
+    # under the same name
     @artist_1_summary = RSpotify::Artist.search(@artist_1)
     @artist_2_summary = RSpotify::Artist.search(@artist_2)
 
+    # 1) Calls first search result for artist AKA "Hardwell" + first result for the artist
+    # 2) Calls followers method to return total number of followers
+    # 3) Saves the number of followers for the first return of the artist.
     @artist_1_followers = @artist_1_summary[0].followers["total"]
     @artist_2_followers = @artist_2_summary[0].followers["total"]
 
-    # @clean_1 = Artist.separate_comma(@artist_1_followers)
+    # Calls the method created under the artist model to separate the number of
+    # followers with a comma to read the number of followers easier
+    @clean_1_number = Artist.separate_comma(@artist_1_followers)
+    @clean_2_number = Artist.separate_comma(@artist_2_followers)
 
-
-  #   @___ = access your business logic (aka model)
-  # ^^^store that in an instance variable
   end
 
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
 
-  def separate_comma(number)
+  def self.separate_comma(number)
     number.to_s.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
   end
 
